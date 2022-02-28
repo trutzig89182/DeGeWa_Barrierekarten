91f4348d13f00f6024279aa819bdba7c9c04fcb0
trutzig89182 <adler.david@freenet.de>
2022-02-28 17:25:22 +0100

    Added path dependency for cards
    The cards shown will be dependend on the altersgruppe selected. If a new altersgruppe is selected, the cover Cards for Wer, Was and Wo are selected.

---
+++ barrierespiel/assets/css/select_age.js
@@ -0,0 +1,16 @@
+document.getElementById("altersgruppe").addEventListener("change", addActivityItem, false);
+
+
+// fuction addActivityItem() {
+//   document.getElementById("myWerPicture").src = "images/Wer/Wer.png";
+//   document.getElementById("myWerPicture").alt = "Wer";
+//   document.getElementById("werCaption").innerHTML = "Wer";
+//   document.getElementById("myWoPicture").src = "images/Wo/Wo.png";
+//   document.getElementById("myWoPicture").alt = "Wo";
+//   document.getElementById("woCaption").innerHTML = "Wo";
+//   document.getElementById("myWasPicture").src = "images/Was/Was.png";
+//   document.getElementById("myWasPicture").alt = "Was";
+//   document.getElementById("wasCaption").innerHTML = "Was";
+// }
+
+window.addEventListener("load", start, false);
--- barrierespiel/assets/js/random.js
+++ barrierespiel/assets/js/random.js
@@ -1,4 +1,5 @@
 document.getElementById("ziehenButton").addEventListener("click", choosePics);
+document.getElementById("altersgruppe").addEventListener("change", addActivityItem, false);
 
 // loads picture paths into arrays
 var werPix = new Array("images/Wer/1.png", "images/Wer/2.png", "images/Wer/3.png", "images/Wer/4.png", "images/Wer/5.png", "images/Wer/6.png", "images/Wer/7.png", "images/Wer/8.png", "images/Wer/9.png", "images/Wer/10.png", "images/Wer/11.png");
@@ -10,18 +11,37 @@ var woPixName = new Array("Pädagogische Einrichtung", "Religiöses Gemeindezent
 var wasPixName = new Array("Offenes Elternfrühstück", "Mutter-Kind-Turnen", "Weihnachtsfeier", "Elternnachmittag", "Elterngespräch", "Basteltag", "Aushang", "Eltern- oder Infobrief", "Informelles Elterngespräch", "Eingewöhnungs- oder Begleitungsphase");
 
 
+function addActivityItem() {
+  document.getElementById("myWerPicture").src = "images/Wer/Wer.png";
+  document.getElementById("myWerPicture").alt = "Wer";
+  document.getElementById("werCaption").innerHTML = "Wer";
+  document.getElementById("myWoPicture").src = "images/Wo/Wo.png";
+  document.getElementById("myWoPicture").alt = "Wo";
+  document.getElementById("woCaption").innerHTML = "Wo";
+  document.getElementById("myWasPicture").src = "images/Was/Was.png";
+  document.getElementById("myWasPicture").alt = "Was";
+  document.getElementById("wasCaption").innerHTML = "Was";
+}
+
 // attributes random cards
 function choosePics() {
-     var randomNum1 = Math.floor(Math.random() * werPix.length);
-     document.getElementById("myWerPicture").src = werPix[randomNum1];
-     document.getElementById("myWerPicture").alt = werPixName[randomNum1];
-     document.getElementById("werCaption").innerHTML = werPixName[randomNum1];
-     var randomNum2 = Math.floor(Math.random() * woPix.length);
-     document.getElementById("myWoPicture").src = woPix[randomNum2];
-     document.getElementById("myWoPicture").alt = woPixName[randomNum2];
-     document.getElementById("woCaption").innerHTML = woPixName[randomNum2];
-     var randomNum3 = Math.floor(Math.random() * wasPix.length);
-     document.getElementById("myWasPicture").src = wasPix[randomNum3];
-     document.getElementById("myWasPicture").alt = wasPixName[randomNum3];
-     document.getElementById("wasCaption").innerHTML = wasPixName[randomNum3];
+    var altersgruppe = document.getElementById("altersgruppe").value;
+    if (altersgruppe == "1") {
+      var randomNum1 = Math.floor(Math.random() * werPix.length);
+      document.getElementById("myWerPicture").src = werPix[randomNum1];
+      document.getElementById("myWerPicture").alt = werPixName[randomNum1];
+      document.getElementById("werCaption").innerHTML = werPixName[randomNum1];
+      var randomNum2 = Math.floor(Math.random() * woPix.length);
+      document.getElementById("myWoPicture").src = woPix[randomNum2];
+      document.getElementById("myWoPicture").alt = woPixName[randomNum2];
+      document.getElementById("woCaption").innerHTML = woPixName[randomNum2];
+      var randomNum3 = Math.floor(Math.random() * wasPix.length);
+      document.getElementById("myWasPicture").src = wasPix[randomNum3];
+      document.getElementById("myWasPicture").alt = wasPixName[randomNum3];
+      document.getElementById("wasCaption").innerHTML = wasPixName[randomNum3];
+    };
+    if (altersgruppe == "2") {
+    }
+    if (altersgruppe == "3") {
+    }
 }
